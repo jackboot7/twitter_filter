805f163ec18278f63b553d4c43ff62be0950f760
Carlos Bruguera <cbruguera@gmail.com>
2013-07-22 18:29:03 -0430

    * Definidos los modelos para el filtrado de tweets y el bloqueo de usuarios

--- tweetfilter/apps/filtering/models.py
+++ tweetfilter/apps/filtering/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 from apps.channels.models import Channel
 
+
 class Trigger(models.Model):
     ACTION_RETWEET = 1
 
@@ -13,3 +14,25 @@ class Trigger(models.Model):
     text = models.CharField(max_length=32)
     action = models.IntegerField(choices=ACTION_CHOICES, default=ACTION_RETWEET)
     channel = models.ForeignKey(Channel)
+
+
+class BannedWord(models.Model):
+    ACTION_BLOCK_TWEET = 1
+    ACTION_BLOCK_USER = 2
+
+    ACTION_CHOICES = (
+        (ACTION_BLOCK_TWEET, "Bloquear tweet"),
+        (ACTION_BLOCK_USER, "Bloquear usuario"),
+        )
+
+    text = models.CharField(max_length=32)
+    action = models.SmallIntegerField(choices=ACTION_CHOICES, default=ACTION_BLOCK_TWEET)
+    channel = models.ForeignKey(Channel)
+
+
+class BlockedUser(models.Model):
+    screen_name = models.CharField(max_length=16)
+    block_date = models.DateTimeField(auto_now=True)
+    block_duration = models.IntegerField()
+    reason = models.CharField(max_length=64)
+
