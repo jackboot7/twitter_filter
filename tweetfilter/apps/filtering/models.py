3c35a164c7ffe01cad5045bffd49b34bd7720da1
Carlos Bruguera <cbruguera@gmail.com>
2013-07-29 17:17:50 -0430

    * Creación del modelo de usuarios permitidos (lista blanca)

--- tweetfilter/apps/filtering/models.py
+++ tweetfilter/apps/filtering/models.py
@@ -29,10 +29,15 @@ class Filter(models.Model):
     action = models.SmallIntegerField(choices=ACTION_CHOICES, default=ACTION_BLOCK_TWEET)
     channel = models.ForeignKey(Channel)
 
-
+# Lista "negra"
 class BlockedUser(models.Model):
     screen_name = models.CharField(max_length=16)
     block_date = models.DateTimeField(auto_now=True)
     block_duration = models.IntegerField()
     reason = models.CharField(max_length=64)
+    channel = models.ForeignKey(Channel)
 
+# Lista "blanca"
+class AllowedUser(models.Model):
+    screen_name = models.CharField(max_length=16)
+    channel = models.ForeignKey(Channel)
