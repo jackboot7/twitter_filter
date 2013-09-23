5b8f0e6b7ee7b1360d9f0924158f3f05e7a9f882
Luis Alberto Santana <jackboot7@gmail.com>
2013-09-23 13:51:45 -0430

    fixed allowed_hosts setting

--- tweetfilter/tweetfilter/settings/common.py
+++ tweetfilter/tweetfilter/settings/common.py
@@ -1,7 +1,6 @@
 # Django settings for tweetfilter project.
 import os
 
-ALLOWED_HOSTS = ['192.168.0.103']
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -34,7 +33,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.103']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
