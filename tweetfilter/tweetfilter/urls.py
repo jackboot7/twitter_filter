55d69093b979e9942f678c2d9add4fe7e28da20f
Luis Alberto Santana <jackboot7@gmail.com>
2013-10-16 11:37:06 -0430

    Changed HomeView to serve as success page, and use ListView generic view.

--- tweetfilter/tweetfilter/urls.py
+++ tweetfilter/tweetfilter/urls.py
@@ -1,24 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from tweetfilter.views import ChannelAddedView
 from views import HomeView
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', HomeView.as_view(), name="home"),
-    url(r'^success$', ChannelAddedView.as_view(), name="channel_added"),
+    url(r'^success$', HomeView.as_view(channel_added="true"), name='channel_added'),
     url(r'^accounts/', include('apps.accounts.urls')),
     url(r'^tasks/', include('djcelery.urls')),
     url(r'^filtering/', include('apps.filtering.urls')),
     url(r'^scheduling/', include('apps.scheduling.urls')),
-    # url(r'^tweetfilter/', include('tweetfilter.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
