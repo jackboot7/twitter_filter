d0fc03d4b75a39c9069948233199b572013051e8
cbruguera <cbruguera@gmail.com>
2014-07-09 11:02:24 -0430

    corrección de indentación en archivo de modelo de hashtags

--- tweetfilter/apps/hashtags/models.py
+++ tweetfilter/apps/hashtags/models.py
@@ -5,10 +5,10 @@ from apps.control.models import ScheduleBlock
 
 
 class HashtagAdvertisement(ScheduleBlock):
-	"""
-	A hashtag intended to serve as advertising. It embeds into tweets as long as there is enough space in it and current time
-	occurs within the scope of the scheduled block defined
-	"""
+    """
+    A hashtag intended to serve as advertising. It embeds into tweets as long as there is enough space in it and current time
+    occurs within the scope of the scheduled block defined
+    """
     channel = models.ForeignKey(Channel, blank=True, null=True)
     text = models.CharField(max_length=32)
     quantity = models.IntegerField()
